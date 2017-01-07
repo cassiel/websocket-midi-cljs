4a0f52f6c8306f6538625aee42762256bed590b6
Nick Rothwell <nick@Goztepe.local>
2017-01-07 21:51:30 +0000

    Latching; DOM update; Bootstrap; video frame.

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -7,12 +7,15 @@
 (def HOST "https://boiling-shore-13036.herokuapp.com/")
 ;;(def HOST "http://localhost:5000")
 
+(def LATCHING true)                     ; note-on to toggle behaviour...
+
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text [:ul [:li "Hello world!"]]}))
+(defonce app-state (atom {:content [:ul [:li "Hello world!"]]
+                          :state {:latch false}}))
 
 (defn hello-world []
-  (:text @app-state))
+  (:content @app-state))
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
@@ -47,14 +50,55 @@
                                       (.stopNote output pitch 1))
                              (js/console.log "other status")))))))))
 
+(defn show-latch [how]
+  (swap! app-state
+         assoc-in [:content 1]
+         [:div.row [:div.col-md-12 [:h2 (if how "ON" "OFF")]]]))
+
+(defn note-on-normal [midi]
+  (.emit socket "to_server" #js {:midi midi})
+  (show-latch true))
+
+(defn note-off-normal [midi]
+  (.emit socket "to_server" #js {:midi midi})
+  (show-latch false))
+
+(defn note-on-latch [midi]
+  (let [status (aget midi 0)
+        pitch (aget midi 1)
+        velocity (aget midi 2)
+        how (get-in (swap! app-state update-in [:state :latch] not)
+                    [:state :latch])
+        ]
+    (js/console.log "latch data" (clj->js [status pitch (if how velocity 0)]))
+    (.emit socket "to_server" (clj->js {:midi [status pitch (if how velocity 0)]}))
+    (show-latch how)))
+
+(defn note-off-latch [midi]
+  )
+
+(def note-on (if LATCHING note-on-latch note-on-normal))
+(def note-off (if LATCHING note-off-latch note-off-normal))
+
 (defn SATELLITE
   "Take MIDI from foot switch, send up to server."
   []
   (when-MIDI (fn [] (when-let [keys (js/WebMidi.getInputByName "LPD8")]
+                      (swap! app-state
+                             assoc :content
+                             [:div
+                              [:div.row [:div.col-md-12 [:h2 "OFF"]]]
+                              [:div.row [:div.col-md-12 [:iframe {:src "http://ipcamlive.com/player/player.php?alias=57c7d74347fa1"
+                                                                  :width "100%"
+                                                                  :height "100%"
+                                                                  :frameBorder 0}]]]
+                              ]
+                             )
+                      (show-latch false)
                       (.addListener keys "noteon"  "all" #(do (js/console.log "noteon" %)
-                                                              (.emit socket "to_server" #js {:midi (.-data %)})))
+                                                              (note-on (.-data %))))
                       (.addListener keys "noteoff" "all" #(do (js/console.log "noteoff" %)
-                                                              (.emit socket "to_server" #js {:midi (.-data %)})))))))
+                                                              (note-off (.-data %))))))))
 
 (defn LIST
   "List of MIDI devices."
@@ -62,9 +106,15 @@
   (when-MIDI #(do
                 (js/console.log "INPUTS"  (.-inputs js/WebMidi))
                 (js/console.log "OUTPUTS" (.-outputs js/WebMidi))
-                (reset! app-state {:text [:ul (map-indexed (fn [i item] [:li {:key i} (.-name item)]) (.-inputs js/WebMidi))]})
-
-                )))
+                (swap! app-state
+                       assoc :content
+                       [:div
+                        [:div.row [:div.col-md-12 [:h2 "INPUTS"]]]
+                        [:div (map-indexed (fn [i item] [:div.row {:key i} [:div.col-md-12 (.-name item)]])
+                                           (.-inputs js/WebMidi))]
+                        [:div.row [:div.col-md-12 [:h2 "OUTPUTS"]]]
+                        [:div (map-indexed (fn [i item] [:div.row {:key i} [:div.col-md-12 (.-name item)]])
+                                           (.-outputs js/WebMidi))]]))))
 
 (.emit socket "to_server" #js {:hello "World"})
 
@@ -73,3 +123,5 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
   )
+
+(SATELLITE)
